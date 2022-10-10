c04bd64927c662a346500b2f1c77a08598b45886
appert <scoreur@gmail.com>
2022-10-10 15:29:37 +0200

    ajout header,et box

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="grid md:grid-cols-2 gap-4" >
+    <!--Box 1-->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+        <h3 class="text-3x1 text-blue-900 font-bold mb-4">Cases</h3>
+        <div class="text-2x1 mb-4">
+            <span class="font-bold">New:</span>
+            {{ stats.NewConfirmed }}
+        </div>
+        <div class="text-2x1 mb-4">
+            <span class="font-bold">Total:</span>
+            {{ stats.TotalConfirmed }}
+        </div>
+    </div>
+    <!--Box 2-->
+    <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+        <h3 class="text-3x1 text-blue-900 font-bold mb-4">Cases</h3>
+        <div class="text-2x1 mb-4">
+            <span class="font-bold">New:</span>
+            {{ stats.NewDeaths }}
+        </div>
+        <div class="text-2x1 mb-4">
+            <span class="font-bold">Total:</span>
+            {{ stats.TotalDeaths }}
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'DataBoxes',
+    props: ['stats']
+}
+
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate"/>
+    <DataBoxes :stats="stats"/>
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3x1 mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt=""/>
+  </main>
+</template>
+
+<script>
+import DataTitle from '@/components/DataTitle.vue'
+import DataBoxes from '@/components/DataBoxes.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    DataTitle,
+    DataBoxes
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+  },
+}
+</script>
