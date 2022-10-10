ddd0d390e18ad36e76b0ad5552f5cd5c77c49c9f
appert <scoreur@gmail.com>
2022-10-10 12:11:12 +0200

    mise en place

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3x1 md:text-5x1 font-bold mb-3"> <i class="fa fa-viruses"></i> Covid-19 Tracker</div>
+    <p>API by <a class="text-blue-300" href="https:/covid19api.com" target="_blank">covid19api.com</a></p>
+  
+  </header>
+
+  
+</template>
+
+<script>
+export default {
+  name: 'Header', 
+}
+</script>
+
