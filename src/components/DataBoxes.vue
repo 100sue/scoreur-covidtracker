67297d27b52af54ce094eb83cf5a287adc26cfdf
appert <scoreur@gmail.com>
2022-10-10 19:33:53 +0200

    ajout des pays et bouton clear

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -5,23 +5,23 @@
         <h3 class="text-3x1 text-blue-900 font-bold mb-4">Cases</h3>
         <div class="text-2x1 mb-4">
             <span class="font-bold">New:</span>
-            {{ stats.NewConfirmed }}
+            {{ numberWithCommas(stats.NewConfirmed )}}
         </div>
         <div class="text-2x1 mb-4">
             <span class="font-bold">Total:</span>
-            {{ stats.TotalConfirmed }}
+            {{ numberWithCommas(stats.TotalConfirmed) }}
         </div>
     </div>
     <!--Box 2-->
     <div class="shadow-md bg-blue-200 p-10 text-center rounded">
-        <h3 class="text-3x1 text-blue-900 font-bold mb-4">Cases</h3>
+        <h3 class="text-3x1 text-blue-900 font-bold mb-4">Deaths</h3>
         <div class="text-2x1 mb-4">
             <span class="font-bold">New:</span>
-            {{ stats.NewDeaths }}
+            {{ numberWithCommas(stats.NewDeaths) }}
         </div>
         <div class="text-2x1 mb-4">
             <span class="font-bold">Total:</span>
-            {{ stats.TotalDeaths }}
+            {{ numberWithCommas(stats.TotalDeaths) }}
         </div>
     </div>
 </div>
@@ -30,7 +30,12 @@
 <script>
 export default {
     name: 'DataBoxes',
-    props: ['stats']
+    props: ['stats'],
+    methods: {
+        numberWithCommas(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        }
+    }
 }
 
 </script>
